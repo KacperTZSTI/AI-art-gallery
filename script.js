7391498c54b468e21f0ec26a09e414310de2dea3
KacperTZSTI <113526043+KacperTZSTI@users.noreply.github.com>
2023-04-06 12:33:29 +0200

    almost finished

--- script.js
+++ script.js
@@ -20,11 +20,47 @@ for(let sect of sectors){
     });
     } 
 
-let sites = ["apple_tree.html", "astronaut.html", "fish_graffiti.html", "ferrari.html", "velociraptor.html"]
+let sites = ["apple_tree.html", "astronaut.html", "fish_graffiti.html", "ferrari.html", "velociraptor.html", "AI.html", "donut.html", "hamster.html", "new_york.html", "astronauts.html", "ferrari.html", "ferrari.html", "ferrari.html", "ferrari.html", "ferrari.html", "ferrari.html", "ferrari.html", "ferrari.html", "ferrari.html", "ferrari.html"]
 
 let rando = document.querySelector(".rando");
 
-rando.addEventListener("click", function(){
-let site = sites[3];
 
+rando.addEventListener("click", function(){
+let random = Math.floor(Math.random() * sites.length - 1);
+let site = sites[random];
+window.open(site, "_self");
+console.log(random);
+console.log(site);
 });
+
+let input = document.getElementById("search");
+let sub = document.getElementById("sub");
+let tags = ["Basic", "Weird", "Painting", "Cute", "Space", "Synthwave"]
+
+
+sub.addEventListener("click", function(){
+    let choice = input.value;
+    if (choice == ""){
+        for(i=0; i<6; i++){
+            if(tags[i]!=choice){
+                let change = document.getElementsByClassName(tags[i]);
+                for(j = 0; j<change.length; j++){
+                    change[j].style.display = "block";
+            }}}
+    }else{
+    input.value ='';
+    for(i=0; i<6; i++){
+        if(tags[i]!=choice){
+            let change = document.getElementsByClassName(tags[i]);
+            for(j = 0; j<change.length; j++){
+                change[j].style.display = "none";
+    
+        }
+    }else{
+        let change = document.getElementsByClassName(tags[i]);
+
+        for(h = 0; h<change.length; h++){
+            change[h].style.display = "block";
+    }
+    }}}});
+
